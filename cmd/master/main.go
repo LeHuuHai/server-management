39432c807bc17bdfe6ed807307f39cda9254a40a
Hari <le31052003@gmail.com>
2026-05-21 11:34:22 +0700

    add master main

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"github.com/LeHuuHai/server-management/api"
-	"github.com/LeHuuHai/server-management/internal/domain/repo"
-	"github.com/LeHuuHai/server-management/internal/file/deserialize"
-	"github.com/LeHuuHai/server-management/internal/file/export"
+	"github.com/LeHuuHai/server-management/config"
 	"github.com/LeHuuHai/server-management/internal/handler"
+	"github.com/LeHuuHai/server-management/internal/infra/app"
+	xlsximport "github.com/LeHuuHai/server-management/internal/infra/file/deserialize"
+	xlsxexport "github.com/LeHuuHai/server-management/internal/infra/file/export"
+	pg "github.com/LeHuuHai/server-management/internal/infra/postgres"
 	"github.com/LeHuuHai/server-management/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	app, err := app.New(cfg)
+	if err != nil {
+		panic(err)
+	}
 
 	// domain
-	serverRepo := repo.NewServerRepository(db)
+	serverRepo := pg.NewServerRepository(app.DB)
 
 	// service
 	serverService := service.NewServerService(serverRepo)
@@ -21,8 +32,8 @@ func main() {
 	//handler
 	serverHandler := handler.NewServerHandler(
 		serverService,
-		&export.ServerXLSXExporter{},
-		&deserialize.ServerXLSXImporter{},
+		xlsxexport.NewServerXLSXExporter(),
+		xlsximport.NewServerXLSXImporter(),
 	)
 
 	// router
