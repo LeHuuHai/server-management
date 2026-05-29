0e0279c7ff8859d299ab49998249f7cea7621e62
Hari <le31052003@gmail.com>
2026-05-29 17:13:00 +0700

    add account repo impl, account, user model

--- internal/model/account.go
+++ internal/model/account.go
@@ -3,9 +3,9 @@ package model
 import authdomain "github.com/LeHuuHai/server-management/internal/domain/auth"
 
 type Account struct {
-	ID       uint
-	UserID   uint
-	UserName string
-	Password string
-	Role     authdomain.Role
+	ID       uint            `gorm:"primaryKey"`
+	UserID   uint            `gorm:"column:user_id;not null"`
+	UserName string          `gorm:"column:user_name;uniqueIndex;not null"`
+	Password string          `gorm:"column:password;not null"`
+	Role     authdomain.Role `gorm:"column:role;type:varchar(50);not null"`
 }
