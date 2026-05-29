20230fc3b7202b1305ddb398c449c21f95ef0315
Hari <le31052003@gmail.com>
2026-05-29 17:53:27 +0700

    rename field

--- internal/model/account.go
+++ internal/model/account.go
@@ -5,7 +5,7 @@ import authdomain "github.com/LeHuuHai/server-management/internal/domain/auth"
 type Account struct {
 	ID       uint            `gorm:"primaryKey"`
 	UserID   uint            `gorm:"column:user_id;not null"`
-	UserName string          `gorm:"column:user_name;uniqueIndex;not null"`
+	Username string          `gorm:"column:username;uniqueIndex;not null"`
 	Password string          `gorm:"column:password;not null"`
 	Role     authdomain.Role `gorm:"column:role;type:varchar(50);not null"`
 }
