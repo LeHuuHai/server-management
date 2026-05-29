39d8e36669f1b0ca570d0a645e1b5b8b4bcfd4e5
Hari <le31052003@gmail.com>
2026-05-29 16:39:35 +0700

    add auth strict

---
+++ internal/middleware/authStrictMiddleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"github.com/LeHuuHai/server-management/api"
+	jwtprovider "github.com/LeHuuHai/server-management/internal/infra/jwt"
+	"github.com/gin-gonic/gin"
+)
+
+var publicOps = map[string]bool{
+	"Login":        true,
+	"RefreshToken": true,
+}
+
+func AuthStrictMiddleware(jwtProvider *jwtprovider.JWTProvider) api.StrictMiddlewareFunc {
+	validToken := NewValidToken(jwtProvider)
+
+	return func(f api.StrictHandlerFunc, operationID string) api.StrictHandlerFunc {
+		return func(ctx *gin.Context, request interface{}) (interface{}, error) {
+			if publicOps[operationID] {
+				return f(ctx, request)
+			}
+
+			// gọi ValidToken
+			validToken(ctx)
+			if ctx.IsAborted() {
+				return nil, nil
+			}
+
+			// gọi ValidScope
+			ValidScope(ctx)
+			if ctx.IsAborted() {
+				return nil, nil
+			}
+
+			return f(ctx, request)
+		}
+	}
+}
